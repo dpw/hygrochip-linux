2795a97de945a1afd1aa3b0f9a833d79bd0df321
Gabriele Martino <g.martino@gmx.com>
2013-10-16 12:49:00 +0200

    Added command-line parameters
    For output filters, continuous output, choose device file.

--- hyt-read.c
+++ hyt-read.c
@@ -113,29 +113,68 @@ static int open_i2c_bus(const char *name)
 	exit(1);
 }
 
+void usage()
+{
+	printf("Usage: hyt-read [ -i seconds ] [ -T | -H ] [ -b i2c bus name | device file ]\n"
+		"\t-b X\tOpen the device named X\n"
+		"\t-i X\tRead data every X seconds\n"
+		"\t-T\tPrint only temperature\n"
+		"\t-H\tPrint only humidity\n"
+		"\t-h\tShow this message\n\n"
+	);
+}
+
 int main(int argc, char **argv)
 {
-	int fd;
+	int fd = 0, c, interval = 0;
+	short ptemp = 1, phum = 1;
 	unsigned int slave = 0x28;
 	ssize_t sz;
 	unsigned char data[4];
 	float humidity, temp;
 
-	if (argc < 1 || argc > 3) {
-		fprintf(stderr,
-			"usage: %s [ i2c bus name ] [ slave address ]\n",
-			argv[0]);
+	while ((c = getopt (argc, argv, "HTb:i:h")) != -1) {
+		switch (c) {
+			case 'b':
+				fd = open_i2c_bus(optarg);
+			break;
+
+			case 'i':
+				interval=atoi(optarg);
+			break;
+
+			case 'T':
+				phum=0;
+				ptemp=1;
+			break;
+
+			case 'H':
+				ptemp=0;
+				phum=1;
+			break;
+
+			case 'h':
+				usage();
+				return 1;
+			break;
+		}
+	}
+	if ((argc-optind)<1){
+		usage();
 		return 1;
 	}
+	/* If the bus name was not specified, argv[optind] is the device file */
+	if (!fd){
+		fd=open_i2c_dev(argv[optind]);
+		optind++;
+	}
 
-	fd = open_i2c_bus(argc >= 2 ? argv[1] : "bcm2708_i2c.1");
-
-	if (argc == 3) {
+	if (argc == optind+1) {
 		char *endptr;
-		long n = strtol(argv[2], &endptr, 0);
+		long n = strtol(argv[optind], &endptr, 0);
 
-		if (endptr == argv[2] || *endptr != 0) {
-			fprintf(stderr, "bad slave address '%s'\n", argv[2]);
+		if (endptr == argv[optind] || *endptr != 0) {
+			fprintf(stderr, "bad slave address '%s'\n", argv[optind]);
 			return 1;
 		}
 
@@ -152,37 +191,45 @@ int main(int argc, char **argv)
 
 	/* initiate reading */
 	data[0] = 0;
-	sz = write(fd, data, 1);
-	if (sz < 0)
-		die_errno("writing to i2c");
+	do {
+		sz = write(fd, data, 1);
+		if (sz < 0)
+			die_errno("writing to i2c");
 
-	/* wait for sensor to provide reading */
-	usleep(60 * 1000);
+		/* wait for sensor to provide reading */
+		usleep(60 * 1000);
 
-	sz = read(fd, data, 4);
-	if (sz < 0)
-		die_errno("reading from i2c");
+		sz = read(fd, data, 4);
+		if (sz < 0)
+			die_errno("reading from i2c");
 
-	if (sz < 4) {
-		fprintf(stderr, "short read (%d bytes)\n", (int)sz);
-		return 1;
-	}
+		if (sz < 4) {
+			fprintf(stderr, "short read (%d bytes)\n", (int)sz);
+			return 1;
+		}
 
-	/*printf("%x %x %x %x\n", data[0], data[1], data[2], data[3]);*/
+		/*printf("%x %x %x %x\n", data[0], data[1], data[2], data[3]);*/
 
-	/*
-	 * Sensor reading are two bytes for humidity, and two bytes
-	 * for temperature, big-endian.  The top two bits of the
-	 * humidity value and the bottom two bits of the temperature
-	 * value are status bits (of undocumented purpose).  Humidity
-	 * readings range from 0 to 100%; temperature readings range
-	 * from -40 to 120 degrees C.  In both cases, the ranges
-	 * correspond to the full range of available bits.
-	 */
-	humidity = ((data[0] & 0x3f) << 8 | data[1]) * (100.0 / 0x3fff);
-	temp = (data[2] << 8 | (data[3] & 0xfc)) * (165.0 / 0xfffc) - 40;
+		/*
+		 * Sensor reading are two bytes for humidity, and two bytes
+		 * for temperature, big-endian.  The top two bits of the
+		 * humidity value and the bottom two bits of the temperature
+		 * value are status bits (of undocumented purpose).  Humidity
+		 * readings range from 0 to 100%; temperature readings range
+		 * from -40 to 120 degrees C.  In both cases, the ranges
+		 * correspond to the full range of available bits.
+		 */
+		humidity = ((data[0] & 0x3f) << 8 | data[1]) * (100.0 / 0x3fff);
+		temp = (data[2] << 8 | (data[3] & 0xfc)) * (165.0 / 0xfffc) - 40;
 
-	printf("%f %f\n", humidity, temp);
+		if (phum)
+			printf("%f ", humidity);
+		if (ptemp)
+			printf("%f", temp);
+		printf("\n");
+
+		sleep(interval);
+	} while (interval > 0);
 
 	close(fd);
 	return 0;
